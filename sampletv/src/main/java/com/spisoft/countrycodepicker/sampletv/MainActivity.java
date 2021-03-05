c311440020ec61117d3651f9bdd151e524e61c13
SPSpisoft <spring_software@yahoo.com>
2021-03-06 01:38:49 +0330

    add fa

--- sampletv/src/main/java/com/spisoft/countrycodepicker/sampletv/MainActivity.java
+++ sampletv/src/main/java/com/spisoft/countrycodepicker/sampletv/MainActivity.java
@@ -20,20 +20,23 @@ public class MainActivity extends Activity {
     setContentView(R.layout.activity_main);
 
     final ImageView im = findViewById(R.id.im);
+
+
+    final CountryCodePicker countryCodePicker = new CountryCodePicker(MainActivity.this);
+    final CountryCodeDialog countryCodeDialog = new CountryCodeDialog(countryCodePicker);
+    countryCodeDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+      @Override
+      public void onDismiss(DialogInterface dialog) {
+        countryCodePicker.getSelectedCountryCode();
+        im.setImageResource(CountryUtils.getFlagDrawableResId(countryCodePicker.getmSelectedCountry()));
+        Toast.makeText(MainActivity.this, countryCodePicker.getSelectedCountryCode(), Toast.LENGTH_SHORT).show();
+      }
+    });
+
     im.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        final CountryCodePicker bb = new CountryCodePicker(MainActivity.this);
-        CountryCodeDialog mm = new CountryCodeDialog(bb);
-        mm.show();
-        mm.setOnDismissListener(new DialogInterface.OnDismissListener() {
-          @Override
-          public void onDismiss(DialogInterface dialog) {
-            bb.getSelectedCountryCode();
-            im.setImageResource(CountryUtils.getFlagDrawableResId(bb.getmSelectedCountry()));
-            Toast.makeText(MainActivity.this, bb.getSelectedCountryCode(), Toast.LENGTH_SHORT).show();
-          }
-        });
+        countryCodeDialog.show();
       }
     });
 
